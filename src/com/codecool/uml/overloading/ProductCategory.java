7c6c28ef06b60df2b926931eb830eab7030d42fc
kinga00099 <kinga00099@gmail.com>
2018-10-17 19:26:52 +0200

    ProductCategory class prepared

--- src/com/codecool/uml/overloading/ProductCategory.java
+++ src/com/codecool/uml/overloading/ProductCategory.java
@@ -1,4 +1,49 @@
 package com.codecool.uml.overloading;
 
-public class ProductCategory {
+import java.util.List;
+
+class ProductCategory {
+    private int id;
+    private String name;
+    private String department;
+    private String description;
+
+    public ProductCategory() {
+    }
+
+    public ProductCategory(String name, String department, String description) {
+    }
+
+    public int getId() {
+        return 0;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Product> getProducts() {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
